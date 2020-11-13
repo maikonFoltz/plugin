547d8c13810eaedf1d1a362dd8ab22c6130c13e1
maikonFoltz <maikonjfoltz@gmail.com>
2020-11-13 07:31:26 -0300

    plugin funcionando

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plugin</title>
+</head>
+<body>
+
+    <input type="text" name="cep" id="cep">
+
+    <script type="text/javascript" src="jquery.min.js"></script>
+    <script type="text/javascript" src="jquery.mask.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
